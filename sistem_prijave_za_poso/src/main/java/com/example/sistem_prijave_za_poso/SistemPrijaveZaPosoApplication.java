6c760a01247b3be061c248e8930b0436e65111d4
Hamza Gačić <hamza.gacic.22@size.ba>
2024-11-02 11:34:18 +0100

    Priprema projekta

---
+++ sistem_prijave_za_poso/src/main/java/com/example/sistem_prijave_za_poso/SistemPrijaveZaPosoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistem_prijave_za_poso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemPrijaveZaPosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemPrijaveZaPosoApplication.class, args);
+	}
+
+}
---
+++ sistem_prijave_za_poso/src/test/java/com/example/sistem_prijave_za_poso/SistemPrijaveZaPosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistem_prijave_za_poso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemPrijaveZaPosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
